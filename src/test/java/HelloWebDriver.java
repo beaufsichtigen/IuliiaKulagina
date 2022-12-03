ef1f34916a9650adcae45203cadb4fd398f89ce0
“Iuliia <Iuliia_Kulagina@epam.com>
2022-12-03 11:34:04 +0100

    Webdriver. First try.

---
+++ src/test/java/HelloWebDriver.java
@@ -0,0 +1,22 @@
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class HelloWebDriver {
+    private static final Logger LOGGER = Logger.getLogger(HelloWebDriver.class);
+    public static void main(String[] args) throws InterruptedException {
+
+
+        WebDriver chromeDriver  = new ChromeDriver();
+        chromeDriver.get("http://ya.ru");
+        WebElement searchYa = chromeDriver.findElement(By.id("text"));
+        searchYa.sendKeys("Кто такой котик Степанида");
+        WebElement searchBtn = chromeDriver.findElement(By.xpath("//*[@type=\"submit\"]"));
+        searchBtn.click();
+        Thread.sleep(10000);
+        chromeDriver.quit();
+    }
+}
